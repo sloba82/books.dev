0da854077bce8c30425f1f07055aaaa70b98f2d1
Dragan Miljanovic <wooolf@sbb.rs>
2016-09-14 12:31:01 +0200

    book and user seeder fix

--- database/seeds/BookSeeder.php
+++ database/seeds/BookSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Book\BookModel;
 
 
 class BookSeeder extends Seeder
@@ -14,18 +15,64 @@ class BookSeeder extends Seeder
     public function run()
     {
         DB::table('books')->truncate();
-        DB::table('books')->insert([
-            'photo'  => 'photo path',
-            'title_eng'  => 'title eng',
-            'title_srb' => 'title srb',
-            'discription_short' => 'discription short',
-            'discription_long' => 'description long',
-            'autor' => 'autor',
-            'price' => 2000,
-            'page_num'      => 1500,
-            'deadline'	=> 'deadline',
-            'book_pdf'      => 'pdf path',
-            'book_cover'	=> 'boock cover',
-        ]);
+
+        $books = [
+            [
+                'photo'  => 'photo path 1',
+                'title_eng'  => 'title eng 1',
+                'title_srb' => 'title srb 1',
+                'discription_short' => 'discription short 1',
+                'discription_long' => 'description long 1',
+                'autor' => 'autor 1',
+                'price' => 2000,
+                'page_num'      => 1500,
+                'deadline'	=> 'deadline 1',
+                'book_pdf'      => 'pdf path 1',
+                'book_cover'	=> 'boock cover 1',
+            ],
+            [
+                'photo'  => 'photo path 2',
+                'title_eng'  => 'title eng 2',
+                'title_srb' => 'title srb 2',
+                'discription_short' => 'discription short 2',
+                'discription_long' => 'description long 2',
+                'autor' => 'autor 2',
+                'price' => 2000,
+                'page_num'      => 1500,
+                'deadline'	=> 'deadline 2',
+                'book_pdf'      => 'pdf path 2',
+                'book_cover'	=> 'boock cover 2',
+            ],
+            [
+                'photo'  => 'photo path 3',
+                'title_eng'  => 'title eng 3',
+                'title_srb' => 'title srb 3',
+                'discription_short' => 'discription short 3',
+                'discription_long' => 'description long 3',
+                'autor' => 'autor 3',
+                'price' => 2000,
+                'page_num'      => 1500,
+                'deadline'	=> 'deadline 3',
+                'book_pdf'      => 'pdf path 3',
+                'book_cover'	=> 'boock cover 3',
+            ],
+            [
+                'photo'  => 'photo path 4',
+                'title_eng'  => 'title eng 4',
+                'title_srb' => 'title srb 4',
+                'discription_short' => 'discription short 4',
+                'discription_long' => 'description long 4',
+                'autor' => 'autor 4',
+                'price' => 2000,
+                'page_num'      => 1500,
+                'deadline'	=> 'deadline 4',
+                'book_pdf'      => 'pdf path 4',
+                'book_cover'	=> 'boock cover 4',
+            ],
+        ];
+
+        foreach ($books as $book) {
+            BookModel::create($book);
+        }
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\User\UserModel;
 
 
 class UserSeeder extends Seeder
@@ -14,16 +15,47 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->truncate();
-        DB::table('users')->insert([
-            'username'  => 'user@user.com',
-            'password'  => md5('user'),
-            'first_name' => 'test',
-            'last_name' => 'test',
-            'address' => 'test',
-            'city' => 'test',
-            'medical_institution' => 'test',
-            'role'      => 1,
-            'active'	=> 1,
-        ]);
+
+        $users = [
+            [
+                'username'  => 'user1@user.com',
+                'password'  => md5('user1'),
+                'first_name' => 'test',
+                'last_name' => 'test',
+                'address' => 'test',
+                'city' => 'test',
+                'medical_institution' => 'test',
+                'role'      => 1,
+                'active'	=> 1,
+            ],
+            [
+
+                'username'  => 'user2@user.com',
+                'password'  => md5('user2'),
+                'first_name' => 'test',
+                'last_name' => 'test',
+                'address' => 'test',
+                'city' => 'test',
+                'medical_institution' => 'test',
+                'role'      => 1,
+                'active'	=> 1,
+            ],
+            [
+
+                'username'  => 'user3@user.com',
+                'password'  => md5('user3'),
+                'first_name' => 'test',
+                'last_name' => 'test',
+                'address' => 'test',
+                'city' => 'test',
+                'medical_institution' => 'test',
+                'role'      => 1,
+                'active'	=> 1,
+            ],
+        ];
+
+        foreach ($users as $user) {
+            UserModel::create($user);
+        }
     }
 }
