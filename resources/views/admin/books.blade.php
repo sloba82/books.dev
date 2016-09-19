77a41b93a338a84f9e30d3a789857b41cc0cdc4c
Dragan Miljanovic <wooolf@sbb.rs>
2016-09-19 17:39:18 +0200

    admin routes and empty blades

--- resources/views/admin/books.blade.php
+++ resources/views/admin/books.blade.php
@@ -1,1 +1,13 @@
 Books!
+
+<div ng-controller="HomeCtrl">
+
+    <div ng-if="isAuthenticated()">
+        <ul ng-init="getAllBooks()">
+            <li ng-repeat="book in books">
+                <a href="#/book/[[book.id]]">
+                    [[book.title_eng]] [[book.title_srb]] [[book.description_short]]</a>
+            </li>
+        </ul>
+    </div>
+</div>
