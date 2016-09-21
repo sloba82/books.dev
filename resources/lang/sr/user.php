230a3e2750292d6bd02f2c672c3ef6e03a74008a
Dragan Miljanovic <wooolf@sbb.rs>
2016-09-21 09:59:06 +0200

    user validation messages translate sr

--- resources/lang/sr/user.php
+++ resources/lang/sr/user.php
@@ -16,6 +16,22 @@ return [
     'not_active_user' => 'Korisnik nije activan.',
     'token_expired' => 'Token je istekao.',
     'token_invalid' => 'Token je nevalidan.',
-    'token_absent' => 'Ne postoji token.'
-
+    'token_absent' => 'Ne postoji token.',
+    'required' => 'Polje je obavezno',
+    'invalid_email' => 'Unesite validnu email adresu',
+    'username_maxlength' => 'Maksimalan broj karaktera je 32',
+    'email_exists' => 'Email mora biti jednistven',
+    'password_minlength' => 'Minimalan broj karaktera je 5',
+    'password_maxlength' => 'Maksimalan broj karaktera je 60',
+    'first_name_minlength' => 'Minimalan broj karaktera je 2',
+    'first_name_maxlength' => 'Maksimalan broj karaktera je 16',
+    'last_name_minlength' => 'Minimalan broj karaktera je 2',
+    'last_name_maxlength' => 'Maksimalan broj karaktera je 32',
+    'address_minlength' => 'Minimalan broj karaktera je 8',
+    'address_maxlength' => 'Maksimalan broj karaktera je 32',
+    'city_minlength' => 'Minimalan broj karaktera je 2',
+    'city_maxlength' => 'Maksimalan broj karaktera je 16',
+    'medical_institution_minlength' => 'Minimalan broj karaktera je 8',
+    'medical_institution_maxlength' => 'Maksimalan broj karaktera je 64'
+    
 ];
